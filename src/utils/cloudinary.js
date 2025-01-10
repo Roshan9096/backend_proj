4d5087fc6f0889d1c4a13e4604566610e6b6ca2f
Roshan9096 <roshanthorat877@gmail.com>
2025-01-11 01:32:08 +0530

    connect with cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localpath) => {
+    try {
+        if(!localpath)return null;
+
+        //upload on cloudinary
+        const responce =await cloudinary.uploader.upload(
+            localpath, {
+                resource_type: "auto",
+            },
+        )
+
+        //file successfully uploaded
+        console.log("file successfully uploaded",responce.url);
+        return responce.url;
+    } catch (error) {
+        fs.unlinkSync(localpath)//remove the locally saved temporary file
+    }
+}
+
+cloudinary.uploader.upload(
+    'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    },
+    function(error, result){console.log(result)}
+    );
