8c6fc19ae58bc15d9af2ff10aa2685186e891b61
Roshan9096 <roshanthorat877@gmail.com>
2025-01-10 14:02:24 +0530

    api error handeling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,39 @@
+/*
+ * Middleware function to handle asynchronous request handlers.
+ * 
+ * This function wraps a request handler in a promise and ensures that any errors
+ * are passed to the next middleware in the stack.
+ * 
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @param {Function} next - The next middleware function in the stack.
+ * 
+ * @returns {void}
+ */
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
