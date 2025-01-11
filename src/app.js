b5d4a41c999371d2d160ccb97adb91fb1dd4c98c
Roshan9096 <roshanthorat877@gmail.com>
2025-01-12 02:23:09 +0530

    write basic route and controller code sole error using gpt

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import express from 'express'
 
 const app = express()
 
@@ -13,5 +13,11 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+//router routes
+import userRouter from './routes/user.routes.js'
+
+//routes declarations
+app.use('/api/v1/users', userRouter)
 
 export { app }
